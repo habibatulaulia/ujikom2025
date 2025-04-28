bcb6192d0a1e5af84d06a3ccb7b70e2ac4f6381f
habibatulaulia <habibatulaulia76@gmail.com>
2025-04-28 09:04:05 +0700

    menambahkan main.js

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
-import { log } from './logger'
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js ";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js"
 
-log('Import works!')
+const firebaseConfig = {
+  apiKey: "AIzaSyBbJDnmlNa2wObKWmQ6MLuXQ5KWbp0FDEA",
+  authDomain: "insan-cemerlang-8011b.firebaseapp.com",
+  projectId: "insan-cemerlang-8011b",
+  storageBucket: "insan-cemerlang-8011b.firebasestorage.app",
+  messagingSenderId: "642542638808",
+  appId: "1:642542638808:web:8dae3c0d85e3be8c5c29ad"
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export async function ambildaftartugas() {
+  const refDokumen = collection(db, "senin");
+  const kueri = query(refDokumen, orderBy("tugas"));
+  const cuplikankueri = await getDocs(kueri);
+
+  let hasil = [];
+  cuplikankueri.forEach((dok) => {
+    hasil.push({
+      id: dok.id,
+      tugas: dok.data().tugas,
+      status: dok.data().status,
+      prioritas: dok.data().prioritas,
+      tanggal: dok.data().tanggal,
+    });
+  });
+
+  return hasil;
+}
+
